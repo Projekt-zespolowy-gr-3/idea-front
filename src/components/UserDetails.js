db8be31376b7b68e5ffe848294e8d186aa581f29
Krystian Kowalski <216808@edu.p.lodz.pl>
2022-05-04 22:11:24 +0200

    Change view of the user details page

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, {useEffect, useState} from 'react';
+import {useTranslation} from 'react-i18next';
 import FetchService from '../services/FetchService';
-import { getUsername } from '../services/UserDataService';
-import { LoadingCss } from '../css/Styles';
+import {getUsername} from '../services/UserDataService';
+import {LoadingCss, useStyles} from '../css/Styles';
 import BeatLoader from 'react-spinners/BeatLoader';
+import {Controller} from "react-hook-form";
+import {Button, TextField} from "@material-ui/core";
 
 export default function UserDetails() {
 
-    const { t } = useTranslation();
+    const {t} = useTranslation();
+    const classes = useStyles();
     const [user, setUser] = useState({});
     const [loading, setLoading] = useState(false);
 
@@ -19,22 +22,59 @@ export default function UserDetails() {
                     setUser(response);
                 }
             }).then(() => {
-                setLoading(false);
-            })
+            setLoading(false);
+        })
     }, [])
 
     if (loading) {
         return (
             <div>
-                <BeatLoader css={LoadingCss} />
+                <BeatLoader css={LoadingCss}/>
             </div>
         )
     } else {
         return (
-            <div>
-                <h2>{t('username')}: {user.login}</h2>
-                <h2>{t('name.surname')}: {user.name} {user.surname}</h2>
-                <h2>{t('email')}: {user.email}</h2>
+            <div className={classes.loginWindow}>
+                <h1>{t('userDetails')}</h1>
+                <TextField
+                    fullWidth
+                    autoFocus
+                    margin="dense"
+                    label={t('username')}
+                    variant="filled"
+                    disabled={true}
+                    value={user.login}
+                />
+                <br/>
+                <TextField
+                    className={classes.loginForm}
+                    fullWidth
+                    margin="dense"
+                    disabled={true}
+                    label={t('name')}
+                    variant="filled"
+                    value={user.name}
+                />
+                <br/>
+                <TextField
+                    className={classes.loginForm}
+                    fullWidth
+                    margin="dense"
+                    label={t('surname')}
+                    variant="filled"
+                    disabled={true}
+                    value={user.surname}
+                />
+                <br/>
+                <TextField
+                    className={classes.loginForm}
+                    fullWidth
+                    disabled={true}
+                    margin="dense"
+                    label={t('email')}
+                    variant="filled"
+                    value={user.email}
+                />
             </div>
         )
     }
